885b11d83b6d9414c251615669cc7f850fa1f066
nnminh-haise <nnminh.sam.1803@gmail.com>
2025-02-18 08:13:57 +0700

    chore: update minor changes on app config module and integrated server port using app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from '@root/app-config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(AppConfigService);
+  const port = configService.port;
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
