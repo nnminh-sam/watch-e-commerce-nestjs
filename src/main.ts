6dd03a28a0990012afa318ba66f66b0a75a273af
nnminh-haise <nnminh.sam.1803@gmail.com>
2025-02-19 10:02:14 +0700

    update: added http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from '@root/app-config/app-config.service';
+import { HttpExceptionFilter } from '@root/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.setGlobalPrefix('api/v1');
 
